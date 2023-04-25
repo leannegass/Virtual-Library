ddad86ed8e50b0f349f4adeaa9605f6e9f23a088
Leanne Gass <leannegass@gmail.com>
2023-04-25 14:35:51 +0100

    added media class

---
+++ classes/Media.js
@@ -0,0 +1,20 @@
+class Media {
+    static totalMediaCount = 0;
+    constructor(title,year,genre,){
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        Media.totalMediaCount += 1;
+    }
+    summary(){
+        return `Title: ${this.title}, Year: ${this.year}, Genre: ${this.genre}`;
+    }
+
+}
+// const book = new Media('The Catcher in the Rye', 1951, 'Fiction');
+// console.log(Media.totalMediaCount); // 1
+// const music = new Media('Abbey Road', 1969, 'Rock');
+// console.log(Media.totalMediaCount); // 2
+// music.summary(); // "Title: Abbey Road, Year: 1969, Genre: Rock"
+
+module.exports = {Media};
